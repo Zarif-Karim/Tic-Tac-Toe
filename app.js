67d303f14b3bc03915d8a440ab2c77690c2e4b0f
Zarif Karim <zarifkarim09@gmail.com>
2022-09-22 12:51:10 +1000

    remove old comments from app.js

--- app.js
+++ app.js
@@ -7,27 +7,3 @@ const server = http.createServer((req,res)=>{
 });
 
 server.listen(5000);
-
-
-
-// function game() {
-
-// let moves = 0;
-// let v = 1;
-// let r = 1;
-// let c = 1;
-// while(moves < 1) 
-// {
-//     board.print();
-//     console.log(`Player ${v++}`);
-    
-//     if(board.update(r,c,v)) moves += 1;
-//     console.log('');
-//     if(v > 2) v = 1;
-// }
-
-
-// console.log("game finished");
-// };
-
-// game();
