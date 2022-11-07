a102a3bf327e41143155907afad02b9459b8007e
Zarif Karim <zarifkarim09@gmail.com>
2022-11-07 15:51:47 +1100

    implemented message displaying on client side:

--- frontend/gridlayout/messaging.js
+++ frontend/gridlayout/messaging.js
@@ -1,10 +1,45 @@
+let username = 'zar123';
+
 const form = get('chat-form');
+const msgInput = get('chat-message-input');
+const msgWindow = get('chat-messages');
 
 form.onsubmit = (event) => {
     event.preventDefault();
-    console.log(event);
+    
+    // console.log(event);
+    if(msgInput.value !== '') {
+        displayMessage(username, msgInput.value);
+
+        //send message to server
+        socket.emit('chat-message', {
+            message : msgInput.value
+        });
+        
+        //empty input
+        msgInput.value = ''; 
+    }
 }
 
-function addMessage(from, message){
-    
+function displayMessage(from, message){
+    // console.log(`${from}: ${message}`)
+    msgWindow.appendChild(createMessage(from,message));    
+    msgWindow.scrollTop = msgWindow.scrollHeight;
+}
+
+function createMessage(username,message) {
+    const cm = document.createElement('div')
+    cm.className = 'chat-message';
+    const un = document.createElement('div')
+    un.className = 'username';
+    const msg = document.createElement('div')
+    msg.className = 'message';
+
+    un.innerText = username;
+    msg.innerText = message;
+
+    cm.appendChild(un);
+    cm.appendChild(msg);    
+
+    return cm;
 }
